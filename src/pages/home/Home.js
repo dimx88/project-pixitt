3e771658c4e8f2a7ecb792c2bedf662e24b367a3
dim <bodhidima@gmail.com>
2022-10-29 16:38:25 +0300

    basic landing page design

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,8 +6,24 @@ import './Home.css';
 export default function Home() {
     return (
         <div className="home">
-            <div className="container">
-                <h1>Welcome to Pixitt</h1>
+            <h1>Welcome to Pixitt</h1>
+            <div className="section">
+                <div className="container">
+                    <h2>Create</h2>
+                    <p>Express yourself with fixed sized pixel drawings, using Pixitt's clean minimalistic interface</p>
+                </div>
+            </div>
+            <div className="section">
+                <div className="container">
+                    <h2>Share</h2>
+                    <p>Share your creations with the world</p>
+                </div>
+            </div>
+            <div className="section">
+                <div className="container">
+                    <h2>Discover</h2>
+                    <p>Watch and get inspired by other people's creations</p>
+                </div>
             </div>
         </div>
     );
