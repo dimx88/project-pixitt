8d79c1ec825762691fe8b6957515b6146033d9c6
dim <bodhidima@gmail.com>
2022-10-30 20:02:50 +0200

    created a context provider for auth

---
+++ src/context/AuthContext.js
@@ -0,0 +1,30 @@
+import { createContext, useReducer } from 'react';
+
+
+export const AuthContext = createContext();
+
+export const authReducer = (state, action) => {
+    switch(action.type) {
+        case 'LOGIN':
+            return;
+        case 'SIGNUP':
+            return;
+        case 'LOGOUT':
+            return;
+        default:
+            return state;
+    }
+};
+
+
+
+export const AuthContextProvider = ({children}) => {
+
+    const [state, dispatch] = useReducer(authReducer, { user: null });
+
+    return (
+        <AuthContext.Provider value={{ ...state, dispatch }}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
