5f573a1f1cae37827af2ca951376e44df2adc33b
dim <bodhidima@gmail.com>
2022-10-31 21:18:03 +0200

    display username in navbar when logged in and hide login/signup buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+
+import { useAuthContext } from "../hooks/useAuthContext";
 import { useLogout } from '../hooks/useLogout';
 
 // Styles
@@ -10,19 +12,32 @@ import './Navbar.css';
 export default function Navbar() {
 
     const { logout, isPending, error } = useLogout();
+    const { user } = useAuthContext();
 
     return (
         <nav className="navbar">
             <div className="container">
-                <div className="logo">
-                    <Link to='/'>
-                        <span className="logo-text">Pixitt</span>
-                        <span>(img)</span>
-                    </Link>
-                </div>
-                <Link to='/login'>Login</Link>
-                <Link to='/signup'>Sign Up</Link>
-                <button className="btn" onClick={logout}>Logout</button>
+                <ul>
+                    <div className="logo">
+                        <Link to='/'>
+                            <span className="logo-text">Pixitt</span>
+                            <span>(img)</span>
+                        </Link>
+                    </div>
+                    {!user &&
+                        <>
+                            <li><Link to='/login'>Login</Link></li>
+                            <li><Link to='/signup'>Sign Up</Link></li>
+                        </>
+                    }
+
+                    {user &&
+                        <>
+                            <p>Hi, <strong>{user.displayName}</strong></p>{!isPending && <button className="btn" onClick={logout}>Logout</button>}
+                            {isPending && <button className="btn" disabled>Logout</button>}
+                        </>
+                    }
+                </ul>
             </div>
         </nav>
     );
