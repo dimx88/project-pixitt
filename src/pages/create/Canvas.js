024658fbeaeb2d435ea5166b24eb7f1a6d74d803
dim <bodhidima@gmail.com>
2022-11-03 21:04:40 +0200

    some improvement to mouse util class

--- src/pages/create/Canvas.js
+++ src/pages/create/Canvas.js
@@ -1,73 +1,57 @@
 
+// Utils
+import Mouse from '../../utils/mouse';
 
 //  Styles
 import { useEffect, useRef, useState } from 'react';
 import './Canvas.css';
 
 export default function Canvas() {
+    // Setup
+    //---------------------------
     const canvasRef = useRef(null);
+    const canvasRef2 = useRef(null);
     const contextRef = useRef(null);
 
-    const height = 600;
-    const width = 800;
+    const dimensions = { width: 800, height: 600 };
 
-    let mouse = { x: 0, y: 0, buttonLeft: false, buttonRight: false, buttonMiddle: false };
+    const mouse = new Mouse(null, true);
 
     useEffect(() => {
         const canvas = canvasRef.current;
         contextRef.current = canvas.getContext('2d');
+        mouse.follow(canvas, true);
     }, []);
 
 
 
-    // window.onmouseup = function(e) { }
+    // ---------------------------------------
 
     const onMouseDown = (e) => {
-        e.preventDefault();
-        if (e.button == 0) mouse.buttonLeft = true;
-        if (e.button == 1) mouse.buttonMiddle = true;
-        if (e.button == 2) mouse.buttonRight = true;
-
 
     };
 
     const onMouseUp = (e) => {
-        if (e.button == 0) mouse.buttonLeft = false;
-        if (e.button == 1) mouse.buttonMiddle = false;
-        if (e.button == 2) mouse.buttonRight = false;
-
-
 
     };
 
     const onMouseMove = (e) => {
-        // setLol(prev => prev + 1);   //enable to retrigger re-render
-        const ctx = contextRef.current;
-        updateMouseCoordinates(e);
-        if (mouse.buttonLeft) ctx.fillRect(mouse.x-8, mouse.y-8, 16, 16);
+
     };
 
     const onMouseLeave = (e) => {
 
     };
 
-    window.oncontextmenu = function(e) {
-        if(e.target.id === "canvas") e.preventDefault();
-    }
 
-    const updateMouseCoordinates = (e) => {
-        const offset = canvasRef.current.getBoundingClientRect();
-        mouse.x = e.nativeEvent.x - offset.left;
-        mouse.y = e.nativeEvent.y - offset.top;
-    }
 
     return (
         <canvas
             className="canvas"
             id="canvas"
             ref={canvasRef}
-            width={width}
-            height={height}
+            width={dimensions.width}
+            height={dimensions.height}
             onMouseDown={onMouseDown}
             onMouseUp={onMouseUp}
             onMouseMove={onMouseMove}
--- src/utils/mouse.js
+++ src/utils/mouse.js
@@ -1,26 +1,67 @@
 export default class Mouse {
-   
-    constructor(element) {
-        this.pos = { x: 0, y: 0 };
-        // this.button = [false, false, false];
-        this.button = { left: false, middle: false, right: false };
-        this.element = element;
 
-        element.addEventListener('mousedown', this.onMouseDown.bind(this));
-        element.addEventListener('mouseup', this.onMouseUp.bind(this));
-        element.addEventListener('mousemove', this.onMouseMove.bind(this));
-        // window.addEventListener('mouseenter', this.onMouseEnter);
-        // window.addEventListener('mouseleave', this.onMouseLeave);
+    constructor(element = null, preventDefault = false) {
+        this.pos = { x: 0, y: 0 };
+        // this.button = [false, false, false]; // Alternative data model
+        this.button = { left: false, middle: false, right: false };
+        this.preventDefault = preventDefault;
+
+        this.element = element && this.follow(element);
+
+        // Commit listener bindings here so that it's possible to remove them later via removeEventListener
+        this._onMouseMove = this.onMouseMove.bind(this);
+        this._onMouseDown = this.onMouseDown.bind(this);
+        this._onMouseUp = this.onMouseUp.bind(this);
+        this._onContextMenu = this.onContextMenu.bind(this);
+
     }
 
+
+    follow(element) {
+        // if same element return, no need to re-follow
+        if (this.element === element) return;
+
+        // Unfollow existing element if one exists
+        if (this.element) this.unfollow();
+
+        element.addEventListener('mousedown', this._onMouseDown);
+        element.addEventListener('mouseup', this._onMouseUp);
+        element.addEventListener('mousemove', this._onMouseMove);
+        element.addEventListener('contextmenu', this._onContextMenu);
+        this.element = element;
+
+        // console.log('mouse following ' + element.id)
+        return element;
+    }
+
+    unfollow() {
+        const element = this.element;
+        element.removeEventListener('mousedown', this._onMouseDown);
+        element.removeEventListener('mouseup', this._onMouseUp);
+        element.removeEventListener('mousemove', this._onMouseMove);
+        element.removeEventListener('contextmenu', this._onContextMenu);
+        this.resetButtons();
+        // console.log('mouse unfollowed');
+    }
+
+    resetButtons() {
+       for (let b in this.button) this.button[b] = false;
+    }
+
+
+
+    onContextMenu(e) {
+        if (this.preventDefault) e.preventDefault()
+    }   
+
     onMouseDown(e) {
-        // this.button[e.button] = true;
+        if (this.preventDefault) e.preventDefault();
+
         this.setButtonState(e.button, true);
 
     }
 
     onMouseUp(e) {
-        // this.button[e.button] = false;  
         this.setButtonState(e.button, false);
 
     }
@@ -29,11 +70,15 @@ export default class Mouse {
         const offset = this.element.getBoundingClientRect();
         this.pos.x = e.clientX - offset.left;
         this.pos.y = e.clientY - offset.top;
+
+        // console.log(this.pos);
     }
-    
+
     setButtonState(button, isDown) {
-        if (button === 0) this.button.left = isDown;
-        if (button === 1) this.button.middle = isDown;
-        if (button === 2) this.button.right = isDown;
+        this.button.left = (button === 0 && isDown) || (this.button.left && button !== 0);
+        this.button.middle = (button === 1 && isDown) || (this.button.middle && button !== 1);
+        this.button.right = (button === 2 && isDown) || (this.button.right && button !== 2);
+
+        //console.log(this.button);
     }
 }
